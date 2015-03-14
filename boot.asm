0dbc745a820948f2b0d2b84531c4ed54b6a4cf74
STRAPTHEBOOTANDFUCKMOOT <HDGHDKSHD@FHFIKDJD.COM>
2015-03-13 21:09:51 -0700

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,1 +1,18 @@
-
+   mov ax, 0x07c0
+   mov ds, ax
+ 
+   mov si, msg
+ch_loop:lodsb
+   or al, al
+   jz hang
+   mov ah, 0x0E
+   int 0x10
+   jmp ch_loop
+ 
+hang:
+   jmp hang
+ 
+msg   db 'Booting...', 12, 10, 0
+   times 510-($-$$) db 0
+   db 0x55
+   db 0xAA
